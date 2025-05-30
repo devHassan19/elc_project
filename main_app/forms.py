13aed0217bc6edab8b947d754815693a1fd2178a
devhassan19 <devhassan19@email.com>
2025-05-30 03:36:03 +0300

    3rd  submit change the form to check box

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,12 +2,16 @@ from django import forms
 from django.core.exceptions import ValidationError
 from .models import StudentRequest, Subject  # Import both models
 
-
-
 class StudentRequestForm(forms.ModelForm):
     class Meta:
         model = StudentRequest
         fields = ['student_name', 'phone_number', 'subject', 'attachment']
+    
+    subject = forms.ModelMultipleChoiceField(
+        queryset=Subject.objects.all(),
+        widget=forms.CheckboxSelectMultiple,  # Use checkboxes
+        label="Subjects"
+    )
     
     attachment = forms.FileField(
         label='Attachment (Image)',
