b488b05d3895c70bbd8decd36bf41c2e39807451
devhassan19 <devhassan19@email.com>
2025-06-04 08:35:03 +0300

    ready to deploy

--- elc/settings.py
+++ elc/settings.py
@@ -102,8 +102,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main_app', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
